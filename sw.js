bae47fddb18c61c37cca5c961bac58e747b19cb6
aquameet-pro <aquameet.pro@gmail.com>
2026-05-22 12:25:38 +0300

    Add PWA: manifest link, apple meta tags, service worker registration and caching

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
-const CACHE = 'aquameet-v1';
+const CACHE = 'aquameet-v2';
+const ASSETS = [
+  '/AquameetPro/',
+  '/AquameetPro/index.html',
+  '/AquameetPro/manifest.json',
+  '/AquameetPro/icon-192.png',
+  '/AquameetPro/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
@@ -14,5 +21,13 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
